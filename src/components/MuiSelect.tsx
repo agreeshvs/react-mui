870a0c8a37aed87ae628cca5a321de67d445fa99
agreeshvs <agreesh777@gmail.com>
2023-09-20 12:39:15 +0530

    Add files via upload

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,19 +1,37 @@
 import React, { useState } from 'react'
 import { Box, TextField, MenuItem, Stack, Typography } from '@mui/material'
 const MuiSelect = () => {
-    const [country,setCountry] = useState('')
-    console.log({country});
+    // const [country,setCountry] = useState('')
+    const [countries,setCountries] = useState<string[]>([])
+    // console.log({country});
+    console.log({countries});    
     
     const handleSelectChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setCountry(event.target.value as string)
+        const value = event.target.value;        
+        setCountries(typeof value == 'string' ? value.split(',') : value)
     }
   return (
     <Box width='250px'>     
         <Stack spacing={2} direction='row'>
+        {/* fullWidth - Make select full width , 
+            other wise width changes with the text or value of select 
+        */}
+        {/* SelectProps={ {
+                    multiple:true
+                }} used to make the select in to multiselect 
+        */}
             <TextField label='Select country' select
-                value={country}
+                value={countries}
                 onChange={handleSelectChange}
                 fullWidth
+                SelectProps={ {
+                    multiple:true
+                }}
+                size='small'
+                color='secondary'
+                helperText='Select country'
+                required
+                error={countries.length == 0}
             >
                 <MenuItem value='IN'>India</MenuItem>
                 <MenuItem value='US'>USA</MenuItem>
